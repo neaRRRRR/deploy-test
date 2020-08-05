28a76aa89703fa21b7d8f76555877603e9d51b62
neaRRRRR <kaancakici510@gmail.com>
2020-08-05 18:17:40 +0300

    Updates

---
+++ precache-manifest.cc8bb6ac48f23c6673f3db300189c98f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762337bfa64b49d215cf52f8d68d4c08",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "cdfb97d5e0fe10bad622",
+    "url": "/deploy-test/static/css/main.42ecc7d1.chunk.css"
+  },
+  {
+    "revision": "c8d0b066adf5e5eabec8",
+    "url": "/deploy-test/static/js/2.05c2530d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/deploy-test/static/js/2.05c2530d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfb97d5e0fe10bad622",
+    "url": "/deploy-test/static/js/main.d078e849.chunk.js"
+  },
+  {
+    "revision": "0aed86fee838f63a4006",
+    "url": "/deploy-test/static/js/runtime-main.54742ce2.js"
+  },
+  {
+    "revision": "229ebabaf1576cbe5507b62fc89a11da",
+    "url": "/deploy-test/static/media/background.229ebaba.webp"
+  },
+  {
+    "revision": "8262dfcc13ff781e6ea1d8a338c265d0",
+    "url": "/deploy-test/static/media/chain.8262dfcc.webp"
+  },
+  {
+    "revision": "d5cec438da58c6a20e7d698d6ea49bb3",
+    "url": "/deploy-test/static/media/foto1.d5cec438.webp"
+  },
+  {
+    "revision": "7b8a2e7f519d412f34e26362ec786edb",
+    "url": "/deploy-test/static/media/foto2.7b8a2e7f.webp"
+  },
+  {
+    "revision": "672401d997c67b16acd8ef95b4a07d2a",
+    "url": "/deploy-test/static/media/foto3.672401d9.webp"
+  },
+  {
+    "revision": "7ff1e135ca251e1a154d72ba0667e815",
+    "url": "/deploy-test/static/media/linkedin.7ff1e135.webp"
+  },
+  {
+    "revision": "c78ded3c3b0b9e76cea5dc1c7844b031",
+    "url": "/deploy-test/static/media/logo.c78ded3c.webp"
+  },
+  {
+    "revision": "992c9d76ddbed277e8cd75d10ca80f77",
+    "url": "/deploy-test/static/media/solutions-img.992c9d76.webp"
+  },
+  {
+    "revision": "a2553be91a97c54d07724738cc564b4a",
+    "url": "/deploy-test/static/media/star.a2553be9.svg"
+  }
+]);
